6fe2224b79cbe55e2f58a10dae1eea05aaaf66ff
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-07-24 00:50:13 +0200

    wip: include embedding model bean

--- jchunk-semantic/src/main/java/jchunk/chunker/semantic/SemanticChunker.java
+++ jchunk-semantic/src/main/java/jchunk/chunker/semantic/SemanticChunker.java
@@ -2,10 +2,17 @@ package jchunk.chunker.semantic;
 
 import jchunk.chunker.core.chunk.Chunk;
 import jchunk.chunker.core.chunk.IChunker;
+import org.springframework.ai.embedding.EmbeddingModel;
 
 import java.util.List;
 
 public class SemanticChunker implements IChunker {
+
+	private final EmbeddingModel embeddingModel;
+
+	public SemanticChunker(EmbeddingModel embeddingModel) {
+		this.embeddingModel = embeddingModel;
+	}
 
 	@Override
 	public List<Chunk> split(String content) {
