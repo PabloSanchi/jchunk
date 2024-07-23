45404559e061b41a77f56082fc25352f1d7ad4e9
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-07-23 23:24:15 +0200

    feat: add jchunk core to semantic module

--- jchunk-semantic/src/main/java/jchunk/chunker/semantic/SemanticChunker.java
+++ jchunk-semantic/src/main/java/jchunk/chunker/semantic/SemanticChunker.java
@@ -1,4 +1,15 @@
 package jchunk.chunker.semantic;
 
-public class SemanticChunker {
+import jchunk.chunker.core.chunk.Chunk;
+import jchunk.chunker.core.chunk.IChunker;
+
+import java.util.List;
+
+public class SemanticChunker implements IChunker {
+
+	@Override
+	public List<Chunk> split(String content) {
+		return null;
+	}
+
 }
