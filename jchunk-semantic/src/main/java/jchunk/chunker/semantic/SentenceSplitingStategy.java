83b1d19bff223a251c4c41d3fc7605737731f47e
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-07-24 17:49:45 +0200

    feat: create splitting sentences enum

---
+++ jchunk-semantic/src/main/java/jchunk/chunker/semantic/SentenceSplitingStategy.java
@@ -0,0 +1,22 @@
+package jchunk.chunker.semantic;
+
+/**
+ * Enum to represent the different strategies to split the sentences
+ * @author Pablo Sanchidrian Herrera
+ */
+public enum SentenceSplitingStategy {
+    DEFAULT("(?<=[.?!])\\s+"),
+    LINE_BREAK("\n"),
+    PARAGRAPH_BREAK("\n\n");
+
+    private String strategy;
+
+    SentenceSplitingStategy(String strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public String toString() {
+        return this.strategy;
+    }
+}
