fe6801dd54a069ca7f3452bf9369b5def4d5e293
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-07-24 11:30:38 +0200

    feat: add code documentation

--- jchunk-core/src/main/java/jchunk/chunker/core/chunk/IChunker.java
+++ jchunk-core/src/main/java/jchunk/chunker/core/chunk/IChunker.java
@@ -2,8 +2,19 @@ package jchunk.chunker.core.chunk;
 
 import java.util.List;
 
+/**
+ * The IChunker interface defines the contract that all chunker implementations must follow.
+ * Implementations of this interface are responsible for splitting a given content string
+ * into a list of Chunk objects.
+ */
 public interface IChunker {
 
+	/**
+	 * Splits the provided content string into a list of Chunk objects.
+	 *
+	 * @param content the content string to be split into chunks
+	 * @return a list of Chunk {@link Chunk} objects representing the split content
+	 */
 	List<Chunk> split(String content);
 
 }
