f796314113f50617e958224ccc19aa81a5b18600
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-08-01 13:15:15 +0200

    feat: add recursive chunker

---
+++ jchunk-recursive-character/src/main/java/jchunk/chunker/recursive/RecursiveCharcterChunker.java
@@ -0,0 +1,15 @@
+package jchunk.chunker.recursive;
+
+import jchunk.chunker.core.chunk.Chunk;
+import jchunk.chunker.core.chunk.IChunker;
+
+import java.util.List;
+
+public class RecursiveCharcterChunker implements IChunker {
+
+	@Override
+	public List<Chunk> split(String content) {
+		return null;
+	}
+
+}
