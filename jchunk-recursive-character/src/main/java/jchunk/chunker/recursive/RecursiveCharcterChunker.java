82a0581c4353b5ac6a1867b122ebd6d733986fb2
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-08-01 17:43:11 +0200

    feat: add constructors and cofig to RecursiveChunker class

--- jchunk-recursive-character/src/main/java/jchunk/chunker/recursive/RecursiveCharcterChunker.java
+++ jchunk-recursive-character/src/main/java/jchunk/chunker/recursive/RecursiveCharcterChunker.java
@@ -7,6 +7,16 @@ import java.util.List;
 
 public class RecursiveCharcterChunker implements IChunker {
 
+	private final Config config;
+
+	public RecursiveCharcterChunker() {
+		this(Config.defaultConfig());
+	}
+
+	public RecursiveCharcterChunker(Config config) {
+		this.config = config;
+	}
+
 	@Override
 	public List<Chunk> split(String content) {
 		return null;
