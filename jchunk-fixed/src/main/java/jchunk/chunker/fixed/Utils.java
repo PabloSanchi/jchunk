8f97f992e81a165f055e6eef48efbe1e45a1e266
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-08-07 18:32:17 +0200

    feat: merge sentences utility

--- jchunk-fixed/src/main/java/jchunk/chunker/fixed/Utils.java
+++ jchunk-fixed/src/main/java/jchunk/chunker/fixed/Utils.java
@@ -5,16 +5,32 @@ import jchunk.chunker.core.chunk.Chunk;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Utils {
 
+	private static final Logger logger = Logger.getLogger(Utils.class.getName());
+
+	public static final String LONGER_THAN_THE_SPECIFIED_ = "Created a chunk of size %d, which is longer than the specified %d";
+
 	/**
 	 * Splits the content into sentences using the delimiter.
 	 * @param content the content to split
+	 * @param config configuration for the chunker/splitter
 	 * @return a list of split sentences
 	 */
-	public static List<String> splitIntoSentences(String content, String delimiter, Config.Delimiter keepDelimiter) {
+	public static List<String> splitIntoSentences(String content, Config config) {
+		String delimiter = config.getDelimiter();
+		Config.Delimiter keepDelimiter = config.getKeepDelimiter();
+
+		if(delimiter.isBlank()) {
+			return content.chars()
+					.mapToObj(c -> String.valueOf((char) c))
+					.collect(Collectors.toList());
+		}
 
 		if (keepDelimiter != Config.Delimiter.NONE) {
 			String withDelimiter = "((?<=%1$s)|(?=%1$s))";
@@ -34,20 +50,69 @@ public class Utils {
 				splits.add(preSplits.getLast());
 			}
 
-			return splits.stream().filter(s -> !s.isBlank()).map(String::trim).toList();
+			return splits.stream().filter(s -> !s.isBlank()).toList();
 		}
 
-		return Arrays.stream(content.split(delimiter)).filter(s -> !s.isBlank()).map(String::trim).toList();
+		return Arrays.stream(content.split(delimiter)).filter(s -> !s.isBlank()).toList();
 	}
 
 	/**
 	 * Merges the sentences into chunks.
 	 * @param sentences the sentences to merge
-	 * @param delimiter the delimiter to use
+	 * @param config configuration for the chunker/splitter
 	 * @return list of chunks
 	 */
-	List<Chunk> mergeSentences(List<String> sentences, String delimiter) {
-		return null;
+	static List<Chunk> mergeSentences(List<String> sentences, Config config) {
+		String delimiter = config.getDelimiter();
+		Integer chunkSize = config.getChunkSize();
+		Integer chunkOverlap = config.getChunkOverlap();
+		Boolean trimWhitespace = config.getTrimWhitespace();
+
+		int currentLen = 0;
+		int delimiterLen = delimiter.length();
+
+		List<Chunk> chunks = new ArrayList<>();
+		List<String> currentChunk = new ArrayList<>();
+
+		AtomicInteger chunkIndex = new AtomicInteger(0);
+
+
+		for (String sentence : sentences) {
+			int sentenceLength = sentence.length();
+
+			if (currentLen + sentenceLength + (currentChunk.isEmpty() ? 0 : delimiterLen) > chunkSize) {
+				if (currentLen > chunkSize) {
+					logger.warning(String.format(LONGER_THAN_THE_SPECIFIED_, currentLen, config.getChunkSize()));
+				}
+
+				if (!currentChunk.isEmpty()) {
+					String generatedSentence = joinSentences(currentChunk, delimiter, trimWhitespace);
+					chunks.add(new Chunk(chunkIndex.getAndIncrement(), generatedSentence));
+
+					while (
+							currentLen > chunkOverlap ||
+							(currentLen + sentenceLength + (currentChunk.isEmpty() ? 0 : delimiterLen) > chunkSize && currentLen > 0)
+					) {
+						currentLen -= currentChunk.removeFirst().length() + (currentChunk.isEmpty() ? 0 : delimiterLen);
+					}
+				}
+			}
+
+			currentChunk.add(sentence);
+			currentLen += sentenceLength + (currentChunk.size() > 1 ? delimiterLen : 0);
+		}
+
+		if (!currentChunk.isEmpty()) {
+			String generatedSentence = joinSentences(currentChunk, config.getDelimiter(), config.getTrimWhitespace());
+			chunks.add(new Chunk(chunkIndex.getAndIncrement(), generatedSentence));
+		}
+
+		return chunks;
+	}
+
+	private static String joinSentences(List<String> sentences, String delimiter, Boolean trimWhitespace) {
+		String generatedSentence = String.join(delimiter, sentences);
+		return trimWhitespace ? generatedSentence.trim() : generatedSentence;
 	}
 
 }
