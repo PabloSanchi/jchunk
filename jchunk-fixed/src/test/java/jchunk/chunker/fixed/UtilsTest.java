4a8a8c667b48ff6b14f7a7bb0f9bbaabae1f8eb3
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-08-07 18:48:25 +0200

    feat: add utils unit tests

---
+++ jchunk-fixed/src/test/java/jchunk/chunker/fixed/UtilsTest.java
@@ -0,0 +1,63 @@
+package jchunk.chunker.fixed;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilsTest {
+
+	private static final String CONTENT = "This is the text I would like to chunk up. It is the example text for this exercise";
+
+	@Test
+	public void testSplitIntoSentencesWithBlanckSeparator() {
+		Config config = Config.builder().delimiter("").build();
+
+		List<String> sentences = Utils.splitIntoSentences(CONTENT, config);
+
+		assertThat(sentences).isNotNull();
+		assertThat(sentences).hasSize(CONTENT.length());
+
+		for (int i = 0; i < CONTENT.length(); i++) {
+			assertThat(sentences.get(i)).isEqualTo(String.valueOf(CONTENT.charAt(i)));
+		}
+	}
+
+	@Test
+	public void testSplitIntoSentencesWithNoDelimiter() {
+		Config config = Config.builder().delimiter("ch").build();
+
+		List<String> sentences = Utils.splitIntoSentences(CONTENT, config);
+
+		assertThat(sentences).isNotNull();
+		assertThat(sentences).hasSize(2);
+		assertThat(sentences.getFirst()).isEqualTo("This is the text I would like to ");
+		assertThat(sentences.getLast()).isEqualTo("unk up. It is the example text for this exercise");
+	}
+
+	@Test
+	public void testSplitIntoSentencesWithDelimiterStart() {
+		Config config = Config.builder().delimiter("ch").keepDelimiter(Config.Delimiter.START).build();
+
+		List<String> sentences = Utils.splitIntoSentences(CONTENT, config);
+
+		assertThat(sentences).isNotNull();
+		assertThat(sentences).hasSize(2);
+		assertThat(sentences.getFirst()).isEqualTo("This is the text I would like to ");
+		assertThat(sentences.getLast()).isEqualTo("chunk up. It is the example text for this exercise");
+	}
+
+	@Test
+	public void testSplitIntoSentencesWithDelimiterEnd() {
+		Config config = Config.builder().delimiter("ch").keepDelimiter(Config.Delimiter.END).build();
+
+		List<String> sentences = Utils.splitIntoSentences(CONTENT, config);
+
+		assertThat(sentences).isNotNull();
+		assertThat(sentences).hasSize(2);
+		assertThat(sentences.getFirst()).isEqualTo("This is the text I would like to ch");
+		assertThat(sentences.getLast()).isEqualTo("unk up. It is the example text for this exercise");
+	}
+
+}
